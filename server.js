c4c2dc4056edb727426f5cac159f73a5d6dea0ea
danieleo1991 <49024672+danieleo1991@users.noreply.github.com>
2025-04-20 13:22:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,35 +3,30 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 
-const { Server } = require('socket.io')(http, {
+const { Server } = require('socket.io'); // poprawnie importujemy klasę Server
+
+const io = new Server(server, {
   cors: {
-    origin: '*', // zezwala na połączenie z dowolnego źródła (np. Twoja gra lokalnie lub z innego hosta)
+    origin: '*',
     methods: ['GET', 'POST']
   }
 });
-
-const io = new Server(server);
 
 const players = {};
 
 io.on('connection', (socket) => {
   console.log(`🟢 Użytkownik połączony: ${socket.id}`);
 
-  // Gdy nowy gracz dołącza
   socket.on('newPlayer', (data) => {
     players[socket.id] = {
       id: socket.id,
       position: data.position
     };
 
-    // Powiadom nowego gracza o innych graczach
     socket.emit('currentPlayers', players);
-
-    // Powiadom innych graczy o nowym graczu
     socket.broadcast.emit('newPlayerJoined', players[socket.id]);
   });
 
-  // Gdy gracz się porusza
   socket.on('updatePosition', (position) => {
     if (players[socket.id]) {
       players[socket.id].position = position;
@@ -39,7 +34,6 @@ io.on('connection', (socket) => {
     }
   });
 
-  // Gdy gracz się rozłącza
   socket.on('disconnect', () => {
     console.log(`🔴 Użytkownik rozłączony: ${socket.id}`);
     delete players[socket.id];
