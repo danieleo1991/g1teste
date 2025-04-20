8dd7f516f2140623062834e1e83cec240d751a1f
danieleo1991 <49024672+danieleo1991@users.noreply.github.com>
2025-04-20 12:39:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const io = require('socket.io')(http, {
 const PORT = process.env.PORT || 3000;
 
 io.on('connection', (socket) => {
-  console.log('✅ Nowy gracz połączony:', socket.id);
+  console.log('✅ Naaaaaaowy gracz połączony:', socket.id);
 
   socket.emit('welcome', 'Witaj w grze online!');
 
