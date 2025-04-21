9bf65bfacc8c9390a4e2c9a95c988a48ea7052e5
danieleo1991 <49024672+danieleo1991@users.noreply.github.com>
2025-04-21 17:54:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ io.on('connection', (socket) => {
 		const projectile = {
 			id: projectile_id,
 			start_position: data.start_position,
-			current_position: { ...start_position },
+			current_position: data.start_position,
 			target_id: data.target_id,
 			target_type: data.target_type
 		};
