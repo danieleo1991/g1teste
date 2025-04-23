5c18872caa8cd188f168af59656c6e07396068ee
danieleo1991 <49024672+danieleo1991@users.noreply.github.com>
2025-04-23 21:18:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -204,7 +204,7 @@ io.on('connection', (socket) => {
 
 		const start_position = {
 			x: attacker.position.x,
-			y: attacker.position.y + 2,
+			y: attacker.position.y + 6,
 			z: attacker.position.z
 		};
 
